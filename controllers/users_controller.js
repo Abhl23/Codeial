75383664fb4ac3c48ce3352a946b1e48cf335729
Abhi <abhishek.microice@gmail.com>
2022-06-02 14:11:59 +0530

    set up views for home and users page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
-module.exports.profile=function(req, res){
-    return res.end('<h1>User Profile</h1>');
+module.exports.users=function(req, res){
+    return res.render('users', {
+        title : "Users"
+    });
 };
