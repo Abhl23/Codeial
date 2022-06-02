0283cd17a68dc1c7ab1b11c0b2025b1eed4e4a21
Abhi <abhishek.microice@gmail.com>
2022-06-02 20:24:38 +0530

    set up mongoDB database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const db=require('./config/mongoose');
 
 // require the layouts library
 const expressLayouts=require('express-ejs-layouts');
