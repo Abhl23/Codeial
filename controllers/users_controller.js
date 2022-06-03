9abe24d9846f7bd2d6fbcbd247298e49868db92f
Abhi <bhatt23abhishek@gmail.com>
2022-06-03 20:32:03 +0530

    write controller action for sign-up form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -22,7 +22,27 @@ module.exports.signUp=function(req, res){
 
 // get the sign up data
 module.exports.create=function(req, res){
+    if(req.body.password!=req.body.confirm_password)
+        return res.redirect('back');
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('Error in finding user while signing up');
+            return;
+        }
 
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('Error in creating user while signing up');
+                    return;
+                }
+
+                return res.redirect('/users/sign-in');
+            });
+        }
+        else
+            return res.redirect('back');
+    })
 };
 
 // sign in and create a session for the user
