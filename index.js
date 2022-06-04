24cc58a27974b87bf2f91e350b157c409289eb55
Abhi <bhatt23abhishek@gmail.com>
2022-06-04 21:07:16 +0530

    authenticate using passport.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app=express();
 const port=8000;
 const db=require('./config/mongoose');
 
+// used for session cookie
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
 
@@ -18,12 +23,26 @@ app.set('layout extractScripts', true);
 
 app.use(express.static('./assets'));
 
-// use the express router
-app.use('/', require('./routes'));
-
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name : 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret : 'blahsomething',
+    saveUninitialized : false,
+    resave : false,
+    cookie : {
+        maxAge : (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use the express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){
