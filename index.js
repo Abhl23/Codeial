85f667b5923cba15798414a6800c7acae6c8f789
Abhi <bhatt23abhishek@gmail.com>
2022-06-06 14:36:01 +0530

    set up scss using node-sass-middleware

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const passportLocal=require('./config/passport-local-strategy');
 const MongoStore=require('connect-mongo');
 
 app.use(sassMiddleware({
-    src : '/assets/scss',
-    dest : '/assets/css',
+    src : './assets/scss',
+    dest : './assets/css',
     debug : true,
     outputStyle : 'extended',
     prefix : '/css'
