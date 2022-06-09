ede1e59e8be530df4eb981617f2681812ee41b5f
Abhi <bhatt23abhishek@gmail.com>
2022-06-09 16:41:30 +0530

    display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,15 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req, res){
-    return res.render('home', {
-        title : "Home"
-    });
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){
+            console.log('Error in fetching posts from the database');
+            return;
+        }
+
+        return res.render('home', {
+            title : 'Home',
+            posts : posts
+        });
+    });        
 };
