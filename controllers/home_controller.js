dd5ceca299984fe985db745ab78aa2cf67f339c0
Abhi <bhatt23abhishek@gmail.com>
2022-06-09 18:49:00 +0530

    create schema for comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,7 @@
 const Post=require('../models/post');
 
 module.exports.home=function(req, res){
+    // populate the user of each post
     Post.find({}).populate('user').exec(function(err, posts){
         if(err){
             console.log('Error in fetching posts from the database');
