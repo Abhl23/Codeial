a0c028d32cf89f23d2a25bc72dcc3cc3e41a7b6d
Abhi <bhatt23abhishek@gmail.com>
2022-06-10 13:51:54 +0530

    display comments and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,16 @@
 const Post=require('../models/post');
 
 module.exports.home=function(req, res){
-    // populate the user of each post
-    Post.find({}).populate('user').exec(function(err, posts){
+    // populate the user and comments of each post
+    Post.find({})
+    .populate('user')
+    .populate({
+        path : 'comments',
+        populate : {            // further populating the user of every comment i.e Nested populating
+            path : 'user'
+        }
+    })
+    .exec(function(err, posts){
         if(err){
             console.log('Error in fetching posts from the database');
             return;
