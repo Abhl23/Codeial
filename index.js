3e18b459ac6e11559d223ad779a1f6ec46e82297
Abhi <bhatt23abhishek@gmail.com>
2022-06-22 19:47:37 +0530

    upload and save the profile picture

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ app.set('layout extractScripts', true);
 
 app.use(express.static('./assets'));
 
+// make the upload path available to the browser
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
