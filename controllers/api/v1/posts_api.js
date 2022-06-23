2826683e7b5b8dd9b1231ddd207bfd03508e251e
Abhi <bhatt23abhishek@gmail.com>
2022-06-23 17:28:58 +0530

    set up an API for listing and deleting a post

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,43 @@
+const Post=require('../../../models/post');
+const Comment=require('../../../models/comment');
+
+module.exports.index=async function(req, res){            // index is usually used when you want to list down something
+
+    let posts=await Post.find({})
+        .sort('-createdAt')
+        .populate('user', 'name')                   // populate the user and comments of each post
+        .populate({
+            path : 'comments',
+            populate : {                    // further populating the user of every comment i.e Nested populating
+                path : 'user',
+                select : 'name'
+            }
+        });
+
+
+    return res.status(200).json({
+        message : 'List of posts',
+        posts : posts
+    });
+};
+
+
+module.exports.destroy=async function(req, res){
+
+    try{
+        let post=await Post.findById(req.params.id);
+
+            post.remove();
+
+            await Comment.deleteMany({post : req.params.id});
+
+
+            return res.status(200).json({
+                message : 'Post and associated comments deleted!'
+            });
+    }catch(err){
+        return res.status(500).json({
+            message : 'Internal Server Error'
+        });
+    }
+};
