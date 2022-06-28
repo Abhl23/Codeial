e261f3584010da4818e6959e7353f9d94d9a1caa
Abhi <bhatt23abhishek@gmail.com>
2022-06-28 17:46:40 +0530

    set up routes and actions for likes

---
+++ controllers/likes_controller.js
@@ -0,0 +1,56 @@
+const Like=require('../models/like');
+const Post=require('../models/post');
+const Comment=require('../models/comment');
+
+module.exports.toggleLike=async function(req, res){
+    try{
+        // find the likeable
+        let likeable;
+        let deleted=false;    // tells you in the frontend if the like has been created or destroyed
+
+        if(req.query.type=='Post'){
+            likeable=await Post.findById(req.query.id).populate('likes');
+        }
+        else{
+            likeable=await Comment.findById(req.query.id).populate('likes');
+        }
+
+        // check if the like already exists
+        let existingLike=await Like.findOne({
+            likeable : req.query.id,
+            onModel : req.query.type,
+            user : req.user._id
+        });
+
+        // if the like already exists then delete it
+        if(existingLike){
+            likeable.likes.pull(existingLike._id);
+            likeable.save();
+
+            existingLike.remove();
+            deleted=true;
+        }
+        else{
+            // else make a new like
+            let newLike=Like.create({
+                user : req.user._id,
+                likeable : req.query.id,
+                onModel : req.query.type
+            });
+
+            likeable.likes.push(newLike._id);
+        }
+
+        return res.status(200).json({
+            message : 'Request Successful!',
+            data : {
+                deleted : deleted
+            }
+        });
+    }catch(err){
+        console.log('Error in toggleLike action:', err);
+        return res.status(500).json({
+            message : 'Internal Server Error'
+        });
+    }
+};
