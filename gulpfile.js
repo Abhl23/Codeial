c8c664023e3ce0edb07f5e9103309ec3427e6dc9
Abhi <bhatt23abhishek@gmail.com>
2022-07-07 17:29:37 +0530

    build the project using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,20 @@ const gulp=require('gulp');
 const sass=require('gulp-sass')(require('sass'));
 const cssnano=require('gulp-cssnano');
 const rev=require('gulp-rev');
+const uglify=require('gulp-uglify-es').default;
+const imagemin=require('gulp-imagemin');
+const del=require('del');
 
-gulp.task('css', function(){
+
+gulp.task('css', function(done){
     console.log('minifying css...');
 
     gulp.src('./assets/scss/**/*.scss')
     .pipe(sass())
     .pipe(cssnano())
-    .pipe(gulp.dest('./assets.css'));
+    .pipe(gulp.dest('./assets/css'));
 
-    return gulp.src('./assets/**/*.css')
+    gulp.src('./assets/**/*.css')
     .pipe(rev())
     .pipe(gulp.dest('./public/assets'))
     .pipe(rev.manifest({
@@ -19,4 +23,53 @@ gulp.task('css', function(){
         merge : true
     }))
     .pipe(gulp.dest('./public/assets'));
+
+    done();
+});
+
+
+gulp.task('js', function(done){
+    console.log('minifying js...');
+    
+    gulp.src('./assets/**/*.js')
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd : 'public',
+        merge : true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+
+    done();
+});
+
+
+gulp.task('images', function(done){
+    console.log('compressing images...');
+
+    gulp.src('./assets/**/*.+(jpg|png|gitf|svg|jpeg)')
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        pwd : 'public',
+        merge : 'true'
+    }))
+    .pipe(gulp.dest('./public/assets'));
+
+    done();
+});
+
+
+// empty the public/assets directory
+gulp.task('clean:assets', function(done){
+    del.sync(['./public/assets'], {force : true});
+    done();
+});
+
+
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+    console.log('building assets...');
+    done();
 });
